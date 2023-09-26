a6063857650e399c94d5de1c33900c321c2478f3
Your Name <76578469+Kumarswamy-palakuri@users.noreply.github.com>
2023-09-27 02:13:09 +0530

    f

---
+++ SSC/10thclass/index.js
@@ -0,0 +1,287 @@
+let All=document.getElementById("All")
+      let chapter1=document.getElementById("chapter1")
+      let chapter2=document.getElementById("chapter2")
+      let chapter3=document.getElementById("chapter3")
+      let chapter4=document.getElementById("chapter4")
+      let chapter5=document.getElementById("chapter5")
+      let chapter6=document.getElementById("chapter6")
+      let chapter7=document.getElementById("chapter7")
+      let chapter8=document.getElementById("chapter8")
+      let chapter9=document.getElementById("chapter9")
+      let chapter10=document.getElementById("chapter10")
+      let chapter11=document.getElementById("chapter11")
+      let chapter12=document.getElementById("chapter12")
+      let chapter13=document.getElementById("chapter13")
+      let chapter14=document.getElementById("chapter14")
+
+      let Textbook=document.getElementById("textbook")
+      let listitem1=document.getElementById("listitem1")
+      let listitem2=document.getElementById("listitem2")
+      let listitem3=document.getElementById("listitem3")
+      let listitem4=document.getElementById("listitem4")
+      let listitem5=document.getElementById("listitem5")
+      let listitem6=document.getElementById("listitem6")
+      let listitem7=document.getElementById("listitem7")
+      let listitem8=document.getElementById("listitem8")
+      let listitem9=document.getElementById("listitem9")
+      let listitem10=document.getElementById("listitem10")
+      let listitem11=document.getElementById("listitem11")
+      let listitem12=document.getElementById("listitem12")
+      let listitem13=document.getElementById("listitem13")
+      let listitem14=document.getElementById("listitem14")
+      All.addEventListener("click", function() {
+        Textbook.style.display = "block";
+        listitem1.style.display = "none";
+        listitem2.style.display = "none";
+        listitem3.style.display = "none";
+        listitem4.style.display = "none";
+        listitem5.style.display = "none";
+        listitem6.style.display = "none";
+        listitem7.style.display = "none";
+        listitem8.style.display = "none";
+        listitem9.style.display = "none";
+        listitem10.style.display = "none";
+        listitem11.style.display = "none";
+        listitem12.style.display = "none";
+        listitem13.style.display = "none";
+        listitem14.style.display = "none";
+    });
+      chapter1.addEventListener("click", function() {
+        Textbook.style.display = "none";
+        listitem1.style.display = "block";
+        listitem2.style.display = "none";
+        listitem3.style.display = "none";
+        listitem4.style.display = "none";
+        listitem5.style.display = "none";
+        listitem6.style.display = "none";
+        listitem7.style.display = "none";
+        listitem8.style.display = "none";
+        listitem9.style.display = "none";
+        listitem10.style.display = "none";
+        listitem11.style.display = "none";
+        listitem12.style.display = "none";
+        listitem13.style.display = "none";
+        listitem14.style.display = "none";
+    });
+
+    chapter2.addEventListener("click", function() {
+      Textbook.style.display = "none";
+        listitem1.style.display = "none";
+        listitem2.style.display = "block";
+        listitem3.style.display = "none";
+        listitem4.style.display = "none";
+        listitem5.style.display = "none";
+        listitem6.style.display = "none";
+        listitem7.style.display = "none";
+        listitem8.style.display = "none";
+        listitem9.style.display = "none";
+        listitem10.style.display = "none";
+        listitem11.style.display = "none";
+        listitem12.style.display = "none";
+        listitem13.style.display = "none";
+        listitem14.style.display = "none";
+  });
+  chapter3.addEventListener("click", function() {
+    Textbook.style.display = "none";
+    listitem1.style.display = "none";
+    listitem2.style.display = "none";
+    listitem3.style.display = "block";
+    listitem4.style.display = "none";
+    listitem5.style.display = "none";
+    listitem6.style.display = "none";
+    listitem7.style.display = "none";
+    listitem8.style.display = "none";
+    listitem9.style.display = "none";
+    listitem10.style.display = "none";
+    listitem11.style.display = "none";
+    listitem12.style.display = "none";
+    listitem13.style.display = "none";
+    listitem14.style.display = "none";
+});
+chapter4.addEventListener("click", function() {
+  Textbook.style.display = "none";
+  listitem1.style.display = "none";
+  listitem2.style.display = "none";
+  listitem3.style.display = "none";
+  listitem4.style.display = "block";
+  listitem5.style.display = "none";
+  listitem6.style.display = "none";
+  listitem7.style.display = "none";
+  listitem8.style.display = "none";
+  listitem9.style.display = "none";
+  listitem10.style.display = "none";
+  listitem11.style.display = "none";
+  listitem12.style.display = "none";
+  listitem13.style.display = "none";
+  listitem14.style.display = "none";
+});
+chapter5.addEventListener("click", function() {
+  Textbook.style.display = "none";
+  listitem1.style.display = "none";
+  listitem2.style.display = "none";
+  listitem3.style.display = "none";
+  listitem4.style.display = "none";
+  listitem5.style.display = "block";
+  listitem6.style.display = "none";
+  listitem7.style.display = "none";
+  listitem8.style.display = "none";
+  listitem9.style.display = "none";
+  listitem10.style.display = "none";
+  listitem11.style.display = "none";
+  listitem12.style.display = "none";
+  listitem13.style.display = "none";
+  listitem14.style.display = "none";
+});
+chapter6.addEventListener("click", function() {
+  Textbook.style.display = "none";
+  listitem1.style.display = "none";
+  listitem2.style.display = "none";
+  listitem3.style.display = "none";
+  listitem4.style.display = "none";
+  listitem5.style.display = "none";
+  listitem6.style.display = "block";
+  listitem7.style.display = "none";
+  listitem8.style.display = "none";
+  listitem9.style.display = "none";
+  listitem10.style.display = "none";
+  listitem11.style.display = "none";
+  listitem12.style.display = "none";
+  listitem13.style.display = "none";
+  listitem14.style.display = "none";
+});
+chapter7.addEventListener("click", function() {
+  Textbook.style.display = "none";
+  listitem1.style.display = "none";
+  listitem2.style.display = "none";
+  listitem3.style.display = "none";
+  listitem4.style.display = "none";
+  listitem5.style.display = "none";
+  listitem6.style.display = "none";
+  listitem7.style.display = "block";
+  listitem8.style.display = "none";
+  listitem9.style.display = "none";
+  listitem10.style.display = "none";
+  listitem11.style.display = "none";
+  listitem12.style.display = "none";
+  listitem13.style.display = "none";
+  listitem14.style.display = "none";
+});
+chapter8.addEventListener("click", function() {
+  Textbook.style.display = "none";
+  listitem1.style.display = "none";
+  listitem2.style.display = "none";
+  listitem3.style.display = "none";
+  listitem4.style.display = "none";
+  listitem5.style.display = "none";
+  listitem6.style.display = "none";
+  listitem7.style.display = "none";
+  listitem8.style.display = "block";
+  listitem9.style.display = "none";
+  listitem10.style.display = "none";
+  listitem11.style.display = "none";
+  listitem12.style.display = "none";
+  listitem13.style.display = "none";
+  listitem14.style.display = "none";
+});
+ chapter9.addEventListener("click", function() {
+  Textbook.style.display = "none";
+  listitem1.style.display = "none";
+  listitem2.style.display = "none";
+  listitem3.style.display = "none";
+  listitem4.style.display = "none";
+  listitem5.style.display = "none";
+  listitem6.style.display = "none";
+  listitem7.style.display = "none";
+  listitem8.style.display = "none";
+  listitem9.style.display = "block";
+  listitem10.style.display = "none";
+  listitem11.style.display = "none";
+  listitem12.style.display = "none";
+  listitem13.style.display = "none";
+  listitem14.style.display = "none";
+});
+chapter10.addEventListener("click", function() {
+  Textbook.style.display = "none";
+  listitem1.style.display = "none";
+  listitem2.style.display = "none";
+  listitem3.style.display = "none";
+  listitem4.style.display = "none";
+  listitem5.style.display = "none";
+  listitem6.style.display = "none";
+  listitem7.style.display = "none";
+  listitem8.style.display = "none";
+  listitem9.style.display = "none";
+  listitem10.style.display = "block";
+  listitem11.style.display = "none";
+  listitem12.style.display = "none";
+  listitem13.style.display = "none";
+  listitem14.style.display = "none";
+});
+chapter11.addEventListener("click", function() {
+  Textbook.style.display = "none";
+  listitem1.style.display = "none";
+  listitem2.style.display = "none";
+  listitem3.style.display = "none";
+  listitem4.style.display = "none";
+  listitem5.style.display = "none";
+  listitem6.style.display = "none";
+  listitem7.style.display = "none";
+  listitem8.style.display = "none";
+  listitem9.style.display = "none";
+  listitem10.style.display = "none";
+  listitem11.style.display = "block";
+  listitem12.style.display = "none";
+  listitem13.style.display = "none";
+  listitem14.style.display = "none";
+});
+chapter12.addEventListener("click", function() {
+  Textbook.style.display = "none";
+  listitem1.style.display = "none";
+  listitem2.style.display = "none";
+  listitem3.style.display = "none";
+  listitem4.style.display = "none";
+  listitem5.style.display = "none";
+  listitem6.style.display = "none";
+  listitem7.style.display = "none";
+  listitem8.style.display = "none";
+  listitem9.style.display = "none";
+  listitem10.style.display = "none";
+  listitem11.style.display = "none";
+  listitem12.style.display = "block";
+  listitem13.style.display = "none";
+  listitem14.style.display = "none";
+});
+chapter13.addEventListener("click", function() {
+  Textbook.style.display = "none";
+  listitem1.style.display = "none";
+  listitem2.style.display = "none";
+  listitem3.style.display = "none";
+  listitem4.style.display = "none";
+  listitem5.style.display = "none";
+  listitem6.style.display = "none";
+  listitem7.style.display = "none";
+  listitem8.style.display = "none";
+  listitem9.style.display = "none";
+  listitem10.style.display = "none";
+  listitem11.style.display = "none";
+  listitem12.style.display = "none";
+  listitem13.style.display = "block";
+  listitem14.style.display = "none";
+});
+chapter14.addEventListener("click", function() {
+  Textbook.style.display = "none";
+  listitem1.style.display = "none";
+  listitem2.style.display = "none";
+  listitem3.style.display = "none";
+  listitem4.style.display = "none";
+  listitem5.style.display = "none";
+  listitem6.style.display = "none";
+  listitem7.style.display = "none";
+  listitem8.style.display = "none";
+  listitem9.style.display = "none";
+  listitem10.style.display = "none";
+  listitem11.style.display = "none";
+  listitem12.style.display = "none";
+  listitem13.style.display = "none";
+  listitem14.style.display = "block";
+});
